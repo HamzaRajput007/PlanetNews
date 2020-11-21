d723a8a28362de811f9085b30e7e33b4a5db3283
HamzaRajput007 <hamzabhai0004@gmail.com>
2020-11-21 15:12:26 +0500

    Splash Ready Completed

--- app/src/main/java/apps/webscare/planetnews/MainActivity.java
+++ app/src/main/java/apps/webscare/planetnews/MainActivity.java
@@ -2,7 +2,14 @@ package apps.webscare.planetnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
